c36246311b89d6f343dbf0a10a467bc6dca5c829
Rodrigo Brito <spensmusic@gmail.com>
2023-10-04 09:44:42 -0300

    fix error status

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     // Joga uma HttpException
-    throw new HttpException('error!', HttpStatus.OK);
+    throw new HttpException('error!', HttpStatus.BAD_REQUEST);
     // return 'Hello World!';
   }
 }
